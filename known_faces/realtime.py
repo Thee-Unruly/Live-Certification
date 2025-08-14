a904c8711da3fa80bad14bb4cd9ebd4e97c1113e
Ibrahim Karisa Fadhili <ibrahim.fadhili@agilebiz.co.ke>
2025-08-14 10:34:52 +0300

    make sure a face was found

--- known_faces/realtime.py
+++ known_faces/realtime.py
@@ -1,7 +1,17 @@
-import face_recognition
-import cv2
+import face_recognition #handles detection + embedding generation
 import os
 
 # Load known faces
 known_face_encodings = []
 known_face_names = []
+
+for filename in os.listdir("known_faces"):
+    path = os.path.join("known_faces", filename)
+    img = face_recognition.load_image_file(path)
+    encodings = face_recognition.face_encodings(img)
+    if encodings:  # Make sure a face was found
+        known_face_encodings.append(encodings[0])
+        name = os.path.splitext(filename)[0]
+        known_face_names.append(name)
+
+
